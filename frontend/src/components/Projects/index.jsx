f1516dee8f094a5c2ee289193a881b1b3cd63e63
Monika Yadav <moniydv@gmail.com>
2024-01-02 10:38:36 -0500

    TODO: link to backend

--- frontend/src/components/Projects/index.jsx
+++ frontend/src/components/Projects/index.jsx
@@ -3,29 +3,30 @@ import ProjectCard from "../ProjectCard";
 import "./style.scss";
 
 function Projects() {
-  const query = `query{
-        allProjects{
-          id
-          image
-          projectDescription
-          codeLink
-          demoLink
-        }
-      }`;
+//   const query = `query{
+//         allProjects{
+//           id
+//           image
+//           projectDescription
+//           codeLink
+//           demoLink
+//         }
+//       }`;
 
   const [projects, setProjects] = useState([]);
 
-  //   useEffect(() => {
-  //     const sendingPost = {
-  //         method: 'POST',
-  //         headers: { 'Content-Type': 'application/json' },
-  //         body: JSON.stringify({query}),
-  //     };
+  //TO DO : connect to backend
+//     useEffect(() => {
+//       const sendingPost = {
+//           method: 'POST',
+//           headers: { 'Content-Type': 'application/json' },
+//           body: JSON.stringify({query}),
+//       };
 
-  //      fetch('https://api-pranavdhar.herokuapp.com/graphql', sendingPost)
-  //         .then(response => response.json())
-  //         .then(data => setProjects(data.data.allProjects));
-  // },[query])
+//        fetch('http://localhost:8000/api/projects', sendingPost)
+//           .then(response => response.json())
+//           .then(data => setProjects(data.data.allProjects));
+//   },[query])
 
   return (
     <div className="projects" id="projects">
@@ -35,7 +36,7 @@ function Projects() {
           <ProjectCard
             key={project.id}
             image={
-              "https://api-pranavdhar.herokuapp.com/media/" + project.image
+              "http:////localhost:8000/media/" + project.image
             }
             description={project.projectDescription}
             codelink={project.codeLink}
